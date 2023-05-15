d5330226522fc2bd9adda929f2672280f34a2aaf
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-05-15 11:44:37 +0300

    Rectify bug in qualification tab when highlighting either work / Education

--- main.js
+++ main.js
@@ -56,14 +56,14 @@ tabs.forEach(tab => {
 
         tabcontents.forEach(tabcontent => {
             tabcontent.classList.remove('qualification_active')
-        })
-        target.classList.add('qualification_active');
+        }),
+        target.classList.add('qualification_active'),
 
-        tabs.forEach(tab => {
-            tab.classList.remove('qualification_active')
-        })
-
-        tabs.classList.add('qualification_active')
+        tabs.forEach(tabs => {
+            tabs.classList.remove('qualification_active')
+        }),
+        tab.classList.add('qualification_active')
+        
     })
 })
 
